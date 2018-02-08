ec8220d612b344e2af78aad1b7fc7db747cd568e
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-08 15:29:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.size == 0  
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli.each_with_index do |index, person|}"
+    puts "The line is currently: #{katz_deli.each_with_index do |person, index|}"
     number = 1
     katz_deli.each do |person|
       print " #{number}. #{person}"
